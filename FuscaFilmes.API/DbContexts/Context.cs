ae88e087f17b2588f618caff971ee0ab9424ec8b
danieldpalma <danielpalma232@gmail.com>
2025-07-12 14:54:03 -0300

    added dbcontext and entityFramework

---
+++ FuscaFilmes.API/DbContexts/Context.cs
@@ -0,0 +1,15 @@
+﻿using FuscaFilmes.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuscaFilmes.API.DbContexts;
+
+public class Context : DbContext
+{
+    DbSet<Movie> Movies { get; set; }
+    DbSet<Director> Directors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=FuscaFilmes.db");
+    }
+}
