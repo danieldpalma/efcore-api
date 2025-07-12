b03f75c387b9459f1083154d05a8e7e0908d811c
danieldpalma <danielpalma232@gmail.com>
2025-07-12 15:04:52 -0300

    fixed access modifier of entities

--- FuscaFilmes.API/DbContexts/Context.cs
+++ FuscaFilmes.API/DbContexts/Context.cs
@@ -5,8 +5,8 @@ namespace FuscaFilmes.API.DbContexts;
 
 public class Context : DbContext
 {
-    DbSet<Movie> Movies { get; set; }
-    DbSet<Director> Directors { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Director> Directors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
