8e74feb21f4d1b200654cb8c9f2b2274147f0957
danieldpalma <danielpalma232@gmail.com>
2025-07-12 15:05:44 -0300

    added get and post endpoint to director

--- FuscaFilmes.API/Program.cs
+++ FuscaFilmes.API/Program.cs
@@ -1,7 +1,24 @@
+using System.Text.Json.Serialization;
+using FuscaFilmes.API.DbContexts;
+using FuscaFilmes.API.Entities;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+using (var context = new Context())
+{
+    context.Database.EnsureCreated();
+};
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.AllowTrailingCommas = true;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 var app = builder.Build();
 
@@ -13,7 +30,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!")
+app.MapGet("/directors", () =>
+    {
+        using var context = new Context();
+        return context.Directors
+            .Include(director => director.Movies)
+            .ToList();
+    })
+    .WithOpenApi();
+
+app.MapPost("/director", (Director director) =>
+    {
+        using var context = new Context();
+        context.Directors.Add(director);
+        context.SaveChanges();
+    })
     .WithOpenApi();
 
 app.Run();
