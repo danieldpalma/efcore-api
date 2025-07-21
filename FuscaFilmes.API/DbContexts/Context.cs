145178521bf6e832d2b6f90c812d3dd7193fff9a
danieldpalma <danielpalma232@gmail.com>
2025-07-21 01:42:08 -0300

    added seed to directors and movies

--- FuscaFilmes.API/DbContexts/Context.cs
+++ FuscaFilmes.API/DbContexts/Context.cs
@@ -17,5 +17,26 @@ public class Context : DbContext
             .WithOne(e => e.Director)
             .HasForeignKey(e => e.DirectorId)
             .IsRequired();
+
+        modelBuilder.Entity<Director>().HasData(
+            new Director { Id = 1, Name = "Christopher Nolan" },
+            new Director { Id = 2, Name = "Steven Spielberg" },
+            new Director { Id = 3, Name = "Quentin Tarantino" },
+            new Director { Id = 4, Name = "Martin Scorsese" },
+            new Director { Id = 5, Name = "Greta Gerwig" }
+        );
+
+        modelBuilder.Entity<Movie>().HasData(
+            new Movie { Id = 1, Title = "Inception", ReleasedYear = 2010, DirectorId = 1 },
+            new Movie { Id = 2, Title = "Interstellar", ReleasedYear = 2014, DirectorId = 1 },
+            new Movie { Id = 3, Title = "Jurassic Park", ReleasedYear = 1993, DirectorId = 2 },
+            new Movie { Id = 4, Title = "Schindler's List", ReleasedYear = 1993, DirectorId = 2 },
+            new Movie { Id = 5, Title = "Pulp Fiction", ReleasedYear = 1994, DirectorId = 3 },
+            new Movie { Id = 6, Title = "Django Unchained", ReleasedYear = 2012, DirectorId = 3 },
+            new Movie { Id = 7, Title = "The Wolf of Wall Street", ReleasedYear = 2013, DirectorId = 4 },
+            new Movie { Id = 8, Title = "Goodfellas", ReleasedYear = 1990, DirectorId = 4 },
+            new Movie { Id = 9, Title = "Lady Bird", ReleasedYear = 2017, DirectorId = 5 },
+            new Movie { Id = 10, Title = "Barbie", ReleasedYear = 2023, DirectorId = 5 }
+        );
     }
 }
---
+++ FuscaFilmes.API/Migrations/20250717182206_SeedInitialData.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FuscaFilmes.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Directors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Christopher Nolan" },
+                    { 2, "Steven Spielberg" },
+                    { 3, "Quentin Tarantino" },
+                    { 4, "Martin Scorsese" },
+                    { 5, "Greta Gerwig" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DirectorId", "ReleasedYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 2010, "Inception" },
+                    { 2, 1, 2014, "Interstellar" },
+                    { 3, 2, 1993, "Jurassic Park" },
+                    { 4, 2, 1993, "Schindler's List" },
+                    { 5, 3, 1994, "Pulp Fiction" },
+                    { 6, 3, 2012, "Django Unchained" },
+                    { 7, 4, 2013, "The Wolf of Wall Street" },
+                    { 8, 4, 1990, "Goodfellas" },
+                    { 9, 5, 2017, "Lady Bird" },
+                    { 10, 5, 2023, "Barbie" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
