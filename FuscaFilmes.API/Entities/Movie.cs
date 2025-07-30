5fbc7b934e71b6070391134ab565e45fb8622148
danieldpalma <danielpalma232@gmail.com>
2025-07-30 09:57:32 -0300

    removed required director id from movies

--- FuscaFilmes.API/Entities/Movie.cs
+++ FuscaFilmes.API/Entities/Movie.cs
@@ -5,6 +5,6 @@ public class Movie
     public int Id { get; set; }
     public required string Title { get; set; }
     public int ReleasedYear { get; set; }
-    public required int DirectorId { get; set; }
+    public int DirectorId { get; set; }
     public Director Director { get; set; } = null!;
 }
