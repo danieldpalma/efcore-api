c5b7ef288d6079b13a444919dbcb5b266e11df5f
danieldpalma <danielpalma232@gmail.com>
2025-08-03 15:25:27 -0300

    added new migration of new arch

---
+++ FuscaFilmes.Repo/Migrations/20250803182319_NewAch.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FuscaFilmes.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewAch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleasedYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    DirectorId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Directors_DirectorId",
+                        column: x => x.DirectorId,
+                        principalTable: "Directors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Directors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Christopher Nolan" },
+                    { 2, "Steven Spielberg" },
+                    { 3, "Quentin Tarantino" },
+                    { 4, "Martin Scorsese" },
+                    { 5, "Greta Gerwig" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DirectorId", "ReleasedYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 2010, "Inception" },
+                    { 2, 1, 2014, "Interstellar" },
+                    { 3, 2, 1993, "Jurassic Park" },
+                    { 4, 2, 1993, "Schindler's List" },
+                    { 5, 3, 1994, "Pulp Fiction" },
+                    { 6, 3, 2012, "Django Unchained" },
+                    { 7, 4, 2013, "The Wolf of Wall Street" },
+                    { 8, 4, 1990, "Goodfellas" },
+                    { 9, 5, 2017, "Lady Bird" },
+                    { 10, 5, 2023, "Barbie" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_DirectorId",
+                table: "Movies",
+                column: "DirectorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Directors");
+        }
+    }
+}
